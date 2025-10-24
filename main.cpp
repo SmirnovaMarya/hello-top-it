4f669e9eff587d47838121aca20210225fa83ef6
SmirnovaMarya <Maria79626957625@yandex.ru>
2025-10-24 22:18:11 +0300

    new line

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 
 int main()
 {
-  std::cout << "Hello, it!";
-  std::cout << "\n";
-<<<<<<< HEAD
   std::cout << "Cool, I am Maria.";
-=======
-  std::cout << "Ok, I am Maria.";
->>>>>>> 351ccdff3de8dc825eb755079fb0d0a3c6bb3f7d
+  return 0;
 }
